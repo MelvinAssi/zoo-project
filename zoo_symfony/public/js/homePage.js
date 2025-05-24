27f211e46b0a2c99029e70b2f4575a8a1d832305
Assi Melvin <melvinassi.pro@gmail.com>
2025-05-24 23:08:29 +0200

    responsive

--- zoo_symfony/public/js/homePage.js
+++ zoo_symfony/public/js/homePage.js
@@ -2,7 +2,53 @@ document.addEventListener('DOMContentLoaded', function () {
     
     const animalList = document.getElementById('animal_list');
     const template = document.getElementById('animal-card-template');
+    const tableBody = document.getElementById('table_body');
 
+
+
+    function formatHour(isoString) {
+        const date = new Date(isoString);
+        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+    }
+    async function getHours() {
+        try {
+           const response = await fetch('/opening-hours', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+           
+            const hours = await response.json();
+
+            hours.forEach(hour => {
+                const tr = document.createElement('tr');
+                const daysCell = document.createElement('td');
+                daysCell.textContent = hour.day || '—';
+                const openingCell = document.createElement('td');
+                openingCell.textContent = hour.openingTime ? formatHour(hour.openingTime) : '—';
+
+                const endingCell = document.createElement('td');
+                endingCell.textContent = hour.closingTime ? formatHour(hour.closingTime) : '—';
+                
+                tr.appendChild(daysCell);
+                tr.appendChild(openingCell);
+                tr.appendChild(endingCell);
+
+                tableBody.appendChild(tr);
+                
+            });
+
+
+
+
+
+        } catch (error) {
+            console.error('Erreur lors de la récupération des horraires :', error);
+        }
+
+    };
     async function getAnimal() {
         try {
            const response = await fetch('/animal/random/4', {
@@ -33,4 +79,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     getAnimal();
+    getHours();
 });
